869b84a9059ddc65b50c79e23f7427e907305dcc
takutoki <34600200+takutoki@users.noreply.github.com>
2021-09-22 13:32:58 +0900

    APレスポンスをデコードする構造体を作成

---
+++ study_english/DataStructure/QuestionResponse.swift
@@ -0,0 +1,26 @@
+//
+//  QuestionResponse.swift
+//  study_english
+//
+//  Created by takumitokiwa on 2021/09/22.
+//
+
+import Foundation
+
+struct QuestionResponse: Codable {
+    
+    let id: Int
+    let question: Question
+    let answers: [Answer]
+    
+    struct Question: Codable {
+        let word: String
+        let question: String
+        let audio: String
+    }
+    
+    struct Answer: Codable {
+        let text: String
+        let correct: Bool
+    }
+}
